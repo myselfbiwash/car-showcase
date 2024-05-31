d911a41c16965cf4593240c3ebb4422e20939635
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-05-31 21:58:57 +0545

    chore: Update Next.js config and package dependencies

--- src/components/component/index.ts
+++ src/components/component/index.ts
@@ -5,6 +5,9 @@ import NavBar1 from "./Navbar1";
 import Hero from "./Hero";
 import Hero1 from "./Hero1";
 import Footer1 from "./Footer1";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
 
-
-export { CustomButton, Footer1, NavBar, NavBar1, Hero, Hero1 };
+export { CustomButton, Footer1, NavBar, NavBar1, Hero, Hero1, Footer, CustomFilter, SearchBar, CarCard, ShowMore };
